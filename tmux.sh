e021da67ace77125acdf442f4a7f009d62676f9a
Daniel Conde <daniel.conde@bbc.co.uk>
2018-06-16 11:57:06 +0100

    fixed tmux restore script

--- tmux.sh
+++ tmux.sh
@@ -1,4 +1,8 @@
-if which tmux >/dev/null 2>&1; then
-        #if not inside a tmux session, and if no session is started, start a new session
-        test -z "$TMUX" && (tmux attach || tmux new-session)
+if [[ -z "$TMUX" ]] ;then
+    ID="$( tmux ls | grep -vm1 attached | cut -d: -f1 )" # get the id of a deattached session
+    if [[ -z "$ID" ]] ;then # if not available create a new one
+        tmux new-session
+    else
+        tmux attach-session -t "$ID" # if available attach to it
+    fi
 fi
